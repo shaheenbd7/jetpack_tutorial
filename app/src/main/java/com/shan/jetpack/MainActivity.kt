6b422da25e9315cb1127dada91f0d1ab777b3de2
S M Shaheen <shaheen.cse20@gmail.com>
2025-05-04 22:15:57 +0600

    JetPack Compose Basics

--- app/src/main/java/com/shan/jetpack/MainActivity.kt
+++ app/src/main/java/com/shan/jetpack/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shan.jetpack.ui.theme.JetpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +27,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Shan",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Apply innerPadding and add additional padding if needed
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                            .fillMaxSize()
+                    ) {
+                        ColumnExample()
+                        Spacer(modifier = Modifier.height(16.dp)) // Add space between components
+                        RowExample()
+                        Spacer(modifier = Modifier.height(16.dp)) // Add space between components
+//                        BoxExample()
+                    }
                 }
             }
         }
+    }
+}
+
+
+
+@Composable
+fun ColumnExample() {
+    Column {
+        Text("Line 1")
+        Text("Line 2")
+    }
+}
+
+@Composable
+fun RowExample() {
+    Row {
+        Text("Left")
+        Spacer(modifier = Modifier.padding(horizontal = 20.dp))
+        Text("Right")
+    }
+}
+
+@Composable
+fun BoxExample() {
+    Box {
+        Text("Bottom Layer")
+        Spacer(modifier = Modifier.padding(20.dp))
+        Text("Top Layer", modifier = Modifier.align(Alignment.Center))
     }
 }
 
@@ -41,7 +85,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
